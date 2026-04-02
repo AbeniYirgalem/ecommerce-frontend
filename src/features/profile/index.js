3c1ea5e9437baeed4d7fd73360492a78bf6fe421
Abeni <abenezeryirgalem0@gmail.com>
2026-04-03 00:25:33 +0300

    feat: add barrel export for profile feature in index.js

---
+++ src/features/profile/index.js
@@ -0,0 +1,7 @@
+﻿// src/features/profile/index.js
+// Barrel export for profile feature.
+
+export { default as ProfileForm } from "../../components/ProfileForm";
+export { default as MerchantProfileForm } from "../../pages/profile/forms/MerchantProfileForm";
+export { default as TutorProfileForm } from "../../pages/profile/forms/TutorProfileForm";
+
