256adee3eb4c09c5dd058c4e0b587b0fe4141434
Abeni <abenezeryirgalem0@gmail.com>
2026-04-03 00:25:44 +0300

    feat: add barrel export for review feature in index.js

---
+++ src/features/review/index.js
@@ -0,0 +1,6 @@
+﻿// src/features/review/index.js
+// Barrel export for review feature.
+
+export { default as ReviewSection } from "../../components/review/ReviewSection";
+export { default as CommentSection } from "../../components/CommentSection";
+
