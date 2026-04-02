9006d1b320e26b0af841fa715492b9bd7c3c3247
Abeni <abenezeryirgalem0@gmail.com>
2026-04-03 00:25:00 +0300

    feat: add barrel export for marketplace-related components in index.js

---
+++ src/features/marketplace/index.js
@@ -0,0 +1,13 @@
+﻿// src/features/marketplace/index.js
+// Barrel export for marketplace-related components.
+// Import from here instead of deep paths:
+//   import { ProductCard } from "@/features/marketplace";
+
+export { default as ProductCard } from "../../components/product/ProductCard";
+export { default as ProductGrid } from "../../components/product/ProductGrid";
+export { default as FeaturedProduct } from "../../components/FeaturedProduct";
+export { default as CategoryButtons } from "../../components/CategoryButtons";
+export { default as CategorySection } from "../../components/CategorySection";
+export { default as TestimonialSection } from "../../components/TestimonialSection";
+export { default as JoinCommunity } from "../../components/JoinCommunity";
+
