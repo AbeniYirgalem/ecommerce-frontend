a60c045d329d31654c199a79ae0e9616577e9525
Abeni <abenezeryirgalem0@gmail.com>
2026-04-06 15:40:40 +0300

    feat: add backward-compatibility shim in uniBazzarApi.js for legacy imports

---
+++ src/redux/api/uniBazzarApi.js
@@ -0,0 +1,7 @@
+﻿// src/redux/api/uniBazzarApi.js
+// WARNING: backward-compatibility shim - do not add logic here.
+// All logic now lives in src/lib/axios.js.
+// This file exists so that any component importing from
+// "../redux/api/uniBazzarApi" or "../../redux/api/uniBazzarApi" continues to work.
+export { injectStore } from "../../lib/axios";
+export { default } from "../../lib/axios";
