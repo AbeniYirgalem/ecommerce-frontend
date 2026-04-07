29ed1b1129cb443946c9259467e6ddd217f38206
Abeni <abenezeryirgalem0@gmail.com>
2026-04-07 15:26:59 +0300

    feat: add PrivateRoute component for authentication guard

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,22 @@
+﻿// src/routes/PrivateRoute.jsx
+// Auth guard -- redirects unauthenticated users to /login.
+// Moved from src/components/PrivateRoute.jsx.
+
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { ROUTES } from "../constants/routes";
+
+/**
+ * @param {{ children: React.ReactNode }} props
+ */
+const PrivateRoute = ({ children }) => {
+  const { isAuthenticated, user } = useSelector((state) => state.auth);
+
+  if (!isAuthenticated || !user) {
+    return <Navigate to={ROUTES.LOGIN} replace />;
+  }
+
+  return children;
+};
+
+export default PrivateRoute;
