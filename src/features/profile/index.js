552a5ef15828a56ca364971247b1288a7a863219
Abeni <abenezeryirgalem0@gmail.com>
2026-04-08 23:10:27 +0300

    fix: remove unused exports for MerchantProfileForm and TutorProfileForm in index.js

--- src/features/profile/index.js
+++ src/features/profile/index.js
@@ -2,6 +2,3 @@
 // Barrel export for profile feature.
 
 export { default as ProfileForm } from "../../components/ProfileForm";
-export { default as MerchantProfileForm } from "../../pages/profile/forms/MerchantProfileForm";
-export { default as TutorProfileForm } from "../../pages/profile/forms/TutorProfileForm";
-
